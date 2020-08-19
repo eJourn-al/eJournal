1c0958994a86f52401eeb6a978fcc7cbb02a8143
Lars van Hijfte <larsvanhijfte@hotmail.com>
2020-08-19 09:52:39 +0200

    Rename migration

--- src/django/VLE/migrations/0048_comment_many_to_many.py
+++ src/django/VLE/migrations/0048_comment_many_to_many.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('VLE', '0048_auto_20200818_1551'),
+        ('VLE', '0047_dynamic_filename'),
     ]
 
     operations = [
