6a105dd25d1e35a6d0cf1b71e3ad83e59808b2c0
M.J. van Keulen <mjvkeulen1@gmail.com>
2020-08-21 12:08:32 +0200

    Display component for multiple number badges

---
+++ src/vue/src/components/assets/NumberBadgesDisplay.vue
@@ -0,0 +1,110 @@
+<template>
+    <div
+        :class="{'absolute': absolute}"
+        class="number-badges-container"
+    >
+        <b-badge
+            v-for="(badge, i) in displayedBadges"
+            :key="`${keyPrefix}-badge-${i}`"
+            v-b-tooltip.hover="('tooltip' in badge) ? tooltipToMsg(badge.tooltip, badge.value) : ''"
+            class="badge-part"
+            :class="{
+                'border-left': i == 0,
+                'border-right': i == displayedBadges.length - 1,
+            }"
+            :style="{ ...colorsStyles[displayedBadges.length - 1 - i] }"
+        >
+            {{ badge.value ? Math.round(badge.value * 100) / 100 : 0 }}
+        </b-badge>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        absolute: {
+            default: true,
+            type: Boolean,
+        },
+        displayZeroValues: {
+            default: false,
+            type: Boolean,
+        },
+        keyPrefix: {
+            required: true,
+        },
+        badges: {
+            required: true,
+            type: Array,
+            default: () => [{
+                value: 0,
+                tooltip: 'Tooltip msg',
+            }],
+            validator: badges => badges.every(badge => 'value' in badge),
+        },
+    },
+    data () {
+        return {
+            colorsStyles: [
+                { 'background-color': '#22648A' }, // $theme-blue
+                { 'background-color': '#FFFFFF', color: '#252C39' },
+                { 'background-color': '#252C39' }, // $theme-dark-blue
+                { 'background-color': '#E8A723' }, // $theme-yellow
+                { 'background-color': '#E64769' }, // $theme-pink
+                { 'background-color': '#473E62' }, // $theme-purple
+                { 'background-color': '#007E33' }, // $theme-green
+                { 'background-color': '#CC0000' }, // $theme-red
+                { 'background-color': '#FF8800' }, // $theme-orange
+            ],
+        }
+    },
+    computed: {
+        /* Reverse so we grow the list from right to left */
+        displayedBadges () {
+            return this.badges.filter(badge => (badge.value === 0 && this.displayZeroValues) || badge.value).reverse()
+        },
+    },
+    methods: {
+        tooltipToMsg (tooltip, n) {
+            if (tooltip === 'needsMarking') { return this.needsMarkingTooltip(n) }
+            if (tooltip === 'unpublished') { return this.unpublishedTooltip(n) }
+            if (tooltip === 'importRequests') { return this.importRequestTooltip(n) }
+            return tooltip
+        },
+        needsMarkingTooltip (n) {
+            return `${n} ${(n > 1) ? 'entries' : 'entry'} need${(n === 1) ? 's' : ''} marking`
+        },
+        unpublishedTooltip (n) {
+            return `${n} grade${(n > 1) ? 's' : ''} need${(n === 1) ? 's' : ''} to be published`
+        },
+        importRequestTooltip (n) {
+            return `${n} outstanding journal import request${(n > 1) ? 's' : ''}`
+        },
+    },
+}
+</script>
+
+<style lang="sass">
+@import '~sass/modules/colors.sass'
+
+.absolute
+    position: absolute
+    right: 0px
+    top: 0px
+
+.number-badges-container
+    font-size: 1em
+    .badge-part
+        border-top: 1px solid $theme-dark-grey !important
+        border-bottom: 1px solid $theme-dark-grey !important
+        border-radius: 0px !important
+        font-size: 1em
+        &.border-left
+            border-left: 1px solid $theme-dark-grey !important
+            border-top-left-radius: 5px !important
+            border-bottom-left-radius: 5px !important
+        &.border-right
+            border-right: 1px solid $theme-dark-grey !important
+            border-top-right-radius: 5px !important
+            border-bottom-right-radius: 5px !important
+</style>
