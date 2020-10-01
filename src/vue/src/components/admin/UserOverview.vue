f8fe92573a0c0db1070ce202a854f52b174ce07e
Engel Hamer <engelhamer@gmail.com>
2020-10-01 22:27:11 +0200

    Show is teacher

--- src/vue/src/components/admin/UserOverview.vue
+++ src/vue/src/components/admin/UserOverview.vue
@@ -8,7 +8,7 @@
     >
         <b-thead>
             <b-tr class="d-flex">
-                <b-th class="col-4">
+                <b-th class="col-3">
                     Name
                 </b-th>
                 <b-th class="col-3">
@@ -18,7 +18,10 @@
                     Email
                 </b-th>
                 <b-th class="col-1">
-                    Status
+                    Teacher
+                </b-th>
+                <b-th class="col-1">
+                    Active
                 </b-th>
                 <b-th class="col-1"/>
             </b-tr>
@@ -29,16 +32,28 @@
                 :key="i"
                 class="d-flex"
             >
-                <b-td class="align-middle col-4">
+                <b-td class="vertical-align-middle col-3">
                     {{ user.full_name }}
                 </b-td>
-                <b-td class="align-middle col-3 username-column">
+                <b-td class="vertical-align-middle col-3 username-column">
                     {{ user.username }}
                 </b-td>
-                <b-td class="align-middle col-3">
+                <b-td class="vertical-align-middle col-3">
                     {{ user.email }}
                 </b-td>
-                <b-td class="align-middle col-1">
+                <b-td class="vertical-align-middle col-1">
+                    <icon
+                        v-if="user.is_teacher"
+                        name="check"
+                        class="text-green"
+                    />
+                    <icon
+                        v-else
+                        name="times"
+                        class="text-grey"
+                    />
+                </b-td>
+                <b-td class="vertical-align-middle col-1">
                     <icon
                         v-if="user.is_active"
                         name="check"
@@ -50,7 +65,7 @@
                         class="text-yellow"
                     />
                 </b-td>
-                <b-td class="align-middle col-1">
+                <b-td class="vertical-align-middle col-1">
                     <b-dropdown
                         lazy
                         noCaret
@@ -63,6 +78,12 @@
                         />
                         <b-dropdown-item-button @click="removeUser(user)">
                             Remove
+                        </b-dropdown-item-button>
+                        <b-dropdown-item-button
+                            v-if="!user.is_teacher"
+                            @click="makeTeacher(user)"
+                        >
+                            Make teacher
                         </b-dropdown-item-button>
                     </b-dropdown>
                 </b-td>
@@ -87,6 +108,9 @@ export default {
     methods: {
         removeUser (user) {
             this.$toasted.info(`TODO: Remove user ${user.full_name}`)
+        },
+        makeTeacher (user) {
+            this.$toasted.info(`TODO: Make teacher ${user.full_name}`)
         },
     },
 }
