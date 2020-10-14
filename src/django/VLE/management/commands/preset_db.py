d47b94c14bfb1957dfbe8988d5db32aeefdc4006
mjvkeulen <39912610+mjvkeulen@users.noreply.github.com>
2020-10-14 09:45:14 +0200

    Addition of a management command to generate variable size courses for performance related testing (#1035)
    * Django command to add a performance course
    
    * Call add performance course command
    
    Co-authored-by: Engel Hamer <39912581+engelhamer@users.noreply.github.com>
    Co-authored-by: Lars <larsvanhijfte@hotmail.com>

--- src/django/VLE/management/commands/preset_db.py
+++ src/django/VLE/management/commands/preset_db.py
@@ -7,11 +7,12 @@ import random
 import test.factory as factory
 
 from dateutil.relativedelta import relativedelta
+from django.core.management import call_command
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from faker import Faker
 
-from VLE.models import Entry, Field, Journal, Node, Role
+from VLE.models import Entry, Journal, Node, Role
 
 faker = Faker()
 
@@ -21,7 +22,7 @@ class Command(BaseCommand):
 
     help = 'Generates useful data for the database.'
 
-    def gen_users(self, n_performance_students):
+    def gen_users(self):
         self.student = factory.Student(
             username='student',
             full_name='Lars van Hijfte',
@@ -87,7 +88,6 @@ class Command(BaseCommand):
         )
 
         self.students = [self.student, self.student2, self.student3, self.student4, self.student5]
-        self.performance_students = [factory.Student() for _ in range(n_performance_students)]
         self.tas = [self.ta, self.ta2]
         self.users = self.students + self.tas + [self.teacher] + [self.test_user] + [self.superuser]
 
@@ -119,18 +119,6 @@ class Command(BaseCommand):
                 'tas': [self.ta2],
                 'student_group_names': ['Algol', 'Ruby']
             },
-            'Performance Course':  {
-                'instance': factory.Course(
-                    name='Performance Course',
-                    abbreviation='SPEED',
-                    author=self.teacher,
-                    startdate=faker.date('2019-09-01'),
-                    enddate=faker.date('2022-07-31'),
-                ),
-                'students': self.students + [self.test_user] + self.performance_students,
-                'tas': [self.ta2],
-                'student_group_names': ['Speedy', 'Gonzales']
-            }
         }
 
         for c in self.courses.values():
@@ -180,16 +168,8 @@ class Command(BaseCommand):
             due_date=timezone.now() + relativedelta(years=1, days=1),
             lock_date=timezone.now() + relativedelta(years=1, days=2),
         )
-        self.performance_assignment = factory.Assignment(
-            name='Performance Assignment',
-            description='<p>User for performance testing, holds all users as expected roles + 400 students</p>',
-            courses=[self.courses['Performance Course']['instance']],
-            format__templates=[{'type': Field.RICH_TEXT}],
-            due_date=timezone.now() + relativedelta(years=1, days=1),
-            lock_date=timezone.now() + relativedelta(years=1, days=2),
-        )
 
-        self.assignments = [self.logboek, self.colloquium, self.group_assignment, self.performance_assignment]
+        self.assignments = [self.logboek, self.colloquium, self.group_assignment]
 
     def gen_group_journals(self):
         """
@@ -211,8 +191,6 @@ class Command(BaseCommand):
 
     def gen_format(self):
         for a in self.assignments:
-            if a.name == self.performance_assignment.name:
-                continue  # Already set to just a text field, reduces the generation time (no files)
             factory.FilesTemplate(format=a.format)
             factory.ColloquiumTemplate(format=a.format)
             factory.MentorgesprekTemplate(format=a.format)
@@ -261,9 +239,6 @@ class Command(BaseCommand):
         # Group Assignment
         factory.ProgressPresetNode(format=self.group_assignment.format, target=10)
 
-        # Performance Assignment
-        factory.ProgressPresetNode(format=self.performance_assignment.format, target=10)
-
     def gen_entries(self):
         for a in self.assignments:
             # NOTE: carefull to use a.journal_set or query via AP, both will yield teacher journals
@@ -303,10 +278,13 @@ class Command(BaseCommand):
         """
         Generates a dummy data for a testing environment.
         """
-        self.gen_users(options['n_performance_students'])
+        self.gen_users()
         self.gen_courses()
         self.gen_assignments()
         self.gen_group_journals()
         self.gen_format()
         self.gen_entries()
         self.gen_journal_import_requests()
+
+        if options['n_performance_students']:
+            call_command('add_performance_course', options['n_performance_students'])
