7fcfa04390e0546653924ce61b343116c8cf9dcb
Engel Hamer <engelhamer@gmail.com>
2020-11-22 16:18:39 +0100

    Added search fields for course and user

--- src/vue/src/components/admin/UserOverview.vue
+++ src/vue/src/components/admin/UserOverview.vue
@@ -1,122 +1,143 @@
 <template>
-    <b-table-simple
-        responsive
-        striped
-        sortBy="name"
-        class="mt-2 mb-0 user-overview"
-    >
-        <b-thead>
-            <b-tr class="d-flex">
-                <b-th class="col-3">
-                    Name
-                </b-th>
-                <b-th class="col-3">
-                    Username
-                </b-th>
-                <b-th class="col-3">
-                    Email
-                </b-th>
-                <b-th class="col-1">
-                    Teacher
-                </b-th>
-                <b-th class="col-1">
-                    Active
-                </b-th>
-                <b-th class="col-1"/>
-            </b-tr>
-        </b-thead>
-        <b-tbody>
-            <b-tr
-                v-for="(user, i) in users"
-                :key="i"
-                class="d-flex"
-            >
-                <b-td
-                    :title="user.full_name"
-                    class="col-3 truncate-content"
+    <load-wrapper :loading="loadingUsers">
+        <b-input
+            v-model="searchValue"
+            class="theme-input full-width mb-2"
+            placeholder="Search by name, username or email"
+        />
+        <b-table-simple
+            responsive
+            striped
+            sortBy="name"
+            class="mt-2 mb-0 user-overview"
+        >
+            <b-thead>
+                <b-tr class="d-flex">
+                    <b-th class="col-3">
+                        Name
+                    </b-th>
+                    <b-th class="col-3">
+                        Username
+                    </b-th>
+                    <b-th class="col-3">
+                        Email
+                    </b-th>
+                    <b-th class="col-1">
+                        Teacher
+                    </b-th>
+                    <b-th class="col-1">
+                        Active
+                    </b-th>
+                    <b-th class="col-1"/>
+                </b-tr>
+            </b-thead>
+            <b-tbody>
+                <b-tr
+                    v-for="(user, i) in filteredUsers"
+                    :key="i"
+                    class="d-flex"
                 >
-                    {{ user.full_name }}
-                </b-td>
-                <b-td
-                    :title="user.username"
-                    class="col-3 truncate-content"
-                >
-                    {{ user.username }}
-                </b-td>
-                <b-td
-                    :title="user.email"
-                    class="col-3 truncate-content"
-                >
-                    {{ user.email }}
-                </b-td>
-                <b-td class="col-1">
-                    <icon
-                        v-if="user.is_teacher"
-                        name="check"
-                        class="text-green"
-                    />
-                    <icon
-                        v-else
-                        name="times"
-                        class="text-grey"
-                    />
-                </b-td>
-                <b-td class="col-1">
-                    <icon
-                        v-if="user.is_active"
-                        name="check"
-                        class="text-green"
-                    />
-                    <icon
-                        v-else
-                        name="history"
-                        class="text-yellow"
-                    />
-                </b-td>
-                <b-td class="col-1">
-                    <b-dropdown
-                        lazy
-                        noCaret
-                        variant="link"
+                    <b-td
+                        :title="user.full_name"
+                        class="col-3 truncate-content"
                     >
+                        {{ user.full_name }}
+                    </b-td>
+                    <b-td
+                        :title="user.username"
+                        class="col-3 truncate-content"
+                    >
+                        {{ user.username }}
+                    </b-td>
+                    <b-td
+                        :title="user.email"
+                        class="col-3 truncate-content"
+                    >
+                        {{ user.email }}
+                    </b-td>
+                    <b-td class="col-1">
                         <icon
-                            slot="button-content"
-                            name="ellipsis-v"
-                            class="move-icon"
+                            v-if="user.is_teacher"
+                            name="check"
+                            class="text-green"
                         />
-                        <b-dropdown-item-button
-                            v-if="user.id !== $store.getters['user/uID']"
-                            @click="removeUser(user)"
-                        >
-                            Remove
-                        </b-dropdown-item-button>
-                        <b-dropdown-item-button
-                            v-if="!user.is_teacher"
-                            @click="makeTeacher(user)"
-                        >
-                            Make teacher
-                        </b-dropdown-item-button>
-                        <b-dropdown-item-button
+                        <icon
                             v-else
-                            @click="removeTeacher(user)"
+                            name="times"
+                            class="text-grey"
+                        />
+                    </b-td>
+                    <b-td class="col-1">
+                        <icon
+                            v-if="user.is_active"
+                            name="check"
+                            class="text-green"
+                        />
+                        <icon
+                            v-else
+                            name="history"
+                            class="text-yellow"
+                        />
+                    </b-td>
+                    <b-td class="col-1">
+                        <b-dropdown
+                            lazy
+                            noCaret
+                            variant="link"
                         >
-                            Remove teacher
-                        </b-dropdown-item-button>
-                    </b-dropdown>
-                </b-td>
-            </b-tr>
-        </b-tbody>
-    </b-table-simple>
+                            <icon
+                                slot="button-content"
+                                name="ellipsis-v"
+                                class="move-icon"
+                            />
+                            <b-dropdown-item-button
+                                v-if="user.id !== $store.getters['user/uID']"
+                                @click="removeUser(user)"
+                            >
+                                Remove
+                            </b-dropdown-item-button>
+                            <b-dropdown-item-button
+                                v-if="!user.is_teacher"
+                                @click="makeTeacher(user)"
+                            >
+                                Make teacher
+                            </b-dropdown-item-button>
+                            <b-dropdown-item-button
+                                v-else
+                                @click="removeTeacher(user)"
+                            >
+                                Remove teacher
+                            </b-dropdown-item-button>
+                        </b-dropdown>
+                    </b-td>
+                </b-tr>
+            </b-tbody>
+        </b-table-simple>
+    </load-wrapper>
 </template>
 
 <script>
 import userAPI from '@/api/user.js'
 
+import loadWrapper from '@/components/loading/LoadWrapper.vue'
+
 export default {
+    components: {
+        loadWrapper,
+    },
     data () {
         return {
             users: [],
+            searchValue: '',
+            loadingUsers: true,
         }
+    },
+    computed: {
+        filteredUsers () {
+            return this.users.filter(user => user.full_name.toLowerCase().includes(this.searchValue.toLowerCase())
+                || user.username.toLowerCase().includes(this.searchValue.toLowerCase())
+                || (user.email && user.email.toLowerCase().includes(this.searchValue.toLowerCase())))
+        },
     },
     created () {
         this.getAllUsers()
@@ -125,6 +146,7 @@ export default {
         getAllUsers () {
             userAPI.getAllUsers()
                 .then((users) => { this.users = users })
+                .finally(() => { this.loadingUsers = false })
         },
         removeUser (user) {
             if (window.confirm(`Are you sure you want to remove ${user.full_name}? All of their work will be deleted.`
