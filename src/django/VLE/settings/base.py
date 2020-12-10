f6d932622ccf09cd79cd50887c56aed3e0b8552d
M.J. van Keulen <mjvkeulen1@gmail.com>
2020-12-10 11:38:16 +0100

    Removed unneeded CI_CD env checks

--- src/django/VLE/settings/base.py
+++ src/django/VLE/settings/base.py
@@ -19,7 +19,7 @@ from sentry_sdk.integrations.celery import CeleryIntegration
 from sentry_sdk.integrations.django import DjangoIntegration
 
 sentry_sdk.init(
-    dsn=None if 'CI_CD' in os.environ else os.environ['SENTRY_DSN'],
+    dsn=os.environ.get('SENTRY_DSN', None),
     integrations=[DjangoIntegration(), CeleryIntegration()],
     release=os.environ['RELEASE_VERSION']
 )
