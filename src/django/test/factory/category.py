6ea9e2214dacbc22afbd9af68f26668337555b51
M.J. van Keulen <mjvkeulen1@gmail.com>
2021-01-27 17:12:49 +0100

    Update faker, use color generation

--- src/django/test/factory/category.py
+++ src/django/test/factory/category.py
@@ -1,5 +1,3 @@
-import colorsys
-import random
 from test.factory.file_context import RichTextCategoryDescriptionFileContextFactory
 
 import factory
@@ -7,39 +5,15 @@ import factory
 import VLE.models
 
 
-def random_bright_HSL_color():
-    """
-    Hue: [0, 1] (In JS this is commonly mapped to [0, 360])
-    Saturation: [0, 1]
-    Lightness: [0, 1]
-    """
-    hue = random.random()
-
-    saturation_floor = 0.8
-    saturation = random.uniform(saturation_floor, 1)
-
-    lightness_floor = 0.4
-    lightness_ceil = 0.8
-    lightness = random.uniform(lightness_floor, lightness_ceil)
-
-    return (hue, saturation, lightness)
-
-
-def random_bright_RGB_color_code():
-    h, s, l = random_bright_HSL_color() # noqa E741
-    r, g, b = [int(256 * i) for i in colorsys.hls_to_rgb(h, l, s)]
-    return '#%x%x%x' % (r, g, b)
-
-
 class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = 'VLE.Category'
 
-    name = factory.Sequence(lambda n: f"{factory.Faker('word').generate()} {n}")
+    name = factory.Sequence(lambda n: f"{factory.Faker('word').generate().capitalize()} {n}")
     description = ''
     assignment = factory.SubFactory('test.factory.assignment.AssignmentFactory')
     author = factory.SelfAttribute('assignment.author')
-    color = factory.LazyFunction(random_bright_RGB_color_code)
+    color = factory.Faker('color', luminosity='bright')
 
     @factory.post_generation
     def templates(self, create, extracted, **kwargs):
