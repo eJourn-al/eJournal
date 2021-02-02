a5fd4d2a09c4125f5b2e0f46b1d63c219337bcb9
M.J. van Keulen <mjvkeulen1@gmail.com>
2021-02-02 13:46:46 +0100

    Increase robustness of validateData mixin

--- src/vue/src/helpers/global_mixins.js
+++ src/vue/src/helpers/global_mixins.js
@@ -5,7 +5,7 @@ export default function initGlobalMixins (Vue) {
                 const invalid = Object.entries(this._data).some((entry) => {
                     const [key, value] = entry
 
-                    if (key.includes('State') && value === false) {
+                    if (key.includes('InputState') && value === false) {
                         const invalidFeedback = this._data[key.replace('InputState', 'InvalidFeedback')]
                         this.$toasted.error(invalidFeedback)
                         return true
